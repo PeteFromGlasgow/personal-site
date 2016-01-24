be8a22027e5509ff3899405d8d25e6c12d3846f8
Peter Kinnaird <peter@peterkinnaird.eu>
2016-01-24 16:44:33 +0000

    I feel bad doing this! commiting lodash because they don't have a bower.json and I don't want to add it as a submodule just yet. Made madlibs work.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,36 @@
+var madlibs = {
+	"adjective": [
+		"Awesome",
+		"Bearded",
+		"Geeky",
+		"Drunk",
+		"Travelling"
+	],
+	"noun": [
+		"Code",
+		"Software",
+		"Website",
+		"Beer",
+		"DIY"
+	],
+	"job": [
+		"Architect",
+		"Creator",
+		"Savent",
+		"Consumer"
+	]
+}
+
+
+
 $(document).ready(function(){
-	$("#main-subtitle").html("Coded Beard Artisan");
+	/**
+	 * Generates a subtitle for the page
+	 * @return {String} Words randomly stuck together
+	 */
+	function madLib(){
+		return _.sample(madlibs.adjective)+" "+_.sample(madlibs.noun)+" "+_.sample(madlibs.job);
+	}
+
+	$("#main-subtitle").html(madLib());
 })
